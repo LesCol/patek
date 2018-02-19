dd9b299f370a761e0a133ad16827355ade0c2690
Christopher F <foxbot@protonmail.com>
2018-02-19 17:23:36 -0500

    Filter should use the Guild Whitelist

--- src/Patek/Options/Filter.cs
+++ src/Patek/Options/Filter.cs
@@ -6,10 +6,11 @@ namespace Patek
     public class Filter
     {
         public bool IsWhitelisted(IChannel channel)
-            => Channels.Contains(channel.Id);
+            => Channels.Contains(channel.Id) || Guilds.Contains((channel as IGuildChannel)?.GuildId ?? 1UL);
         public bool IsElevated(IUser user)
             => Users.Contains(user.Id);
 
+        public ulong[] Guilds { get; set; } = new ulong[0];
         public ulong[] Channels { get; set; } = new ulong[0];
         public ulong[] Users { get; set; } = new ulong[0];
     }
